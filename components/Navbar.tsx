7302b0e0384f3bb77c12dc6316a33847a7a49cf6
Waqar Ahmad <codewithwaqarahmad@gmail.com>
2023-11-03 10:39:39 -0700

    Initial Commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,17 @@
+import { Logo } from ".";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <Logo />
+
+      <ul>Home</ul>
+      <ul>About</ul>
+      <ul>Skills</ul>
+      <ul>Projects</ul>
+      <ul>Contact</ul>
+    </nav>
+  );
+};
+
+export default Navbar;
