17664f0223443ce6caaf8ea25e2623196e88dd95
Waqar Ahmad <codewithwaqarahmad@gmail.com>
2023-11-09 23:40:14 -0800

    feat: projects added

--- lib/projects.ts
+++ lib/projects.ts
@@ -1,44 +1,44 @@
-import { TechIconsProps } from ".";
-import logos from "./logos";
+// import { TechIconsProps } from ".";
+// import logos from "./logos";
 
-type projectNames =
-  | "theWildOasis"
-  | "worldWise"
-  | "fastReactPizza"
-  | "usePopCorn"
-  | "travelList"
-  | "eatNSplit"
-  | "thisWebsite"
-  | "reactQuiz";
+// type projectNames =
+//   | "theWildOasis"
+//   | "worldWise"
+//   | "fastReactPizza"
+//   | "usePopCorn"
+//   | "travelList"
+//   | "eatNSplit"
+//   | "thisWebsite"
+//   | "reactQuiz";
 
-// type StackType = { label: string; src: string, wid};
+// // type StackType = { label: string; src: string, wid};
 
-export type ProjectType = {
-  title: string;
-  description: string[];
-  image: {
-    main?: { src: string; width: number; height: number };
-    webm?: string;
-    poster?: string;
-    vidHeight?: number;
-    vidWidth?: number;
-  };
-  links: { github: string; demo: string };
-  stack: TechIconsProps[];
-};
+// export type ProjectType = {
+//   title: string;
+//   description: string[];
+//   image: {
+//     main?: { src: string; width: number; height: number };
+//     webm?: string;
+//     poster?: string;
+//     vidHeight?: number;
+//     vidWidth?: number;
+//   };
+//   links: { github: string; demo: string };
+//   stack: TechIconsProps[];
+// };
 
-type Projects = { [K in projectNames]: ProjectType };
+// type Projects = { [K in projectNames]: ProjectType };
 
-const projects: Projects = {
-  theWildOasis: {
-    title: "The Wild Oasis",
-    description: [
-      "A social networking twitter like web app. Users can **create** posts, **like** posts and **comment** on posts.",
-      "User can visit any **user profile** by clicking at their name or avatar to view posts by them.",
-      "End-to-End typesafe code",
-    ],
-    image: { main: { src: "/", width: 800, height: 800 } },
-    stack: [ logos.react, logos.reactquery, logos.css, ]
-  },
-};
-export default projects;
+// const projects: Projects = {
+//   theWildOasis: {
+//     title: "The Wild Oasis",
+//     description: [
+//       "A social networking twitter like web app. Users can **create** posts, **like** posts and **comment** on posts.",
+//       "User can visit any **user profile** by clicking at their name or avatar to view posts by them.",
+//       "End-to-End typesafe code",
+//     ],
+//     image: { main: { src: "/", width: 800, height: 800 } },
+//     stack: [ logos.react, logos.reactQuery, logos.css, ]
+//   },
+// };
+// export default projects;
