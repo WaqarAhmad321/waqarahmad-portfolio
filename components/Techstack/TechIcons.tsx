b5b396c06f28824dd004eddf7d0a25076d822893
Waqar Ahmad <codewithwaqarahmad@gmail.com>
2023-11-18 21:03:55 +0500

    Update TechIcons component

--- components/Techstack/TechIcons.tsx
+++ components/Techstack/TechIcons.tsx
@@ -22,15 +22,25 @@ const TechIcons = ({ index, label, src, width, height }: TechIconsProps) => {
   const renderIcons = () => {
     return (
       <div className="m-2 flex flex-col items-center">
-        <p className="mb-2 mt-2 whitespace-nowrap text-sm">{label}</p>
+        <p className="mb-2 mt-2 whitespace-nowrap text-xs sm:text-sm">
+          {label}
+        </p>
 
-        <Image
-          src={src}
-          alt={`${label} icon`}
-          width={width}
-          height={height}
-          className={`transition-transform hover:scale-125`}
-        />
+        <div className="flex h-16 w-16 items-center justify-center">
+          <Image
+            src={src}
+            alt={`${label} icon`}
+            width={60}
+            height={60}
+            className={`h-auto ${
+              label?.toLowerCase() === "css" ? "w-11 sm:w-14" : "w-12 sm:w-20"
+            } ${
+              label?.toLowerCase() === "tailwind"
+                ? "w-24 sm:w-32"
+                : "w-12 sm:w-20"
+            } transition-transform hover:scale-125`}
+          />
+        </div>
       </div>
     );
   };
--- lib/logos.ts
+++ lib/logos.ts
@@ -18,8 +18,6 @@ type Logos = {
   [K in logoNames]: {
     label: string;
     src: string;
-    width: number;
-    height: number;
   };
 };
 
@@ -27,173 +25,117 @@ const logos: Logos = {
   typescript: {
     label: "TypeScript",
     src: "/techstack/typescript.svg",
-    width: 60,
-    height: 60,
   },
   react: {
     label: "React",
     src: "/techstack/react.svg",
-    width: 60,
-    height: 60,
   },
   nextjs: {
     label: "Next.js",
     src: "/techstack/nextjs.svg",
-    width: 60,
-    height: 60,
   },
   javascript: {
     label: "JavaScript",
     src: "/techstack/javascript.svg",
-    width: 60,
-    height: 60,
   },
   tailwind: {
     label: "Tailwind",
     src: "/techstack/tailwind-css.svg",
-    width: 90,
-    height: 90,
   },
   reactQuery: {
     label: "React Query",
     src: "/techstack/react-query.svg",
-    width: 65,
-    height: 65,
   },
   supabase: {
     label: "Supabase",
     src: "/techstack/supabase.svg",
-    width: 55,
-    height: 30,
   },
   redux: {
     label: "Redux",
     src: "/techstack/redux.svg",
-    width: 55,
-    height: 30,
   },
   reactRouter: {
     label: "React Router",
     src: "/techstack/react-router.svg",
-    width: 60,
-    height: 30,
   },
   git: {
     label: "Git",
     src: "/techstack/git.svg",
-    width: 55,
-    height: 30,
   },
   framerMotion: {
     label: "Framer Motion",
     src: "/techstack/framer-motion.svg",
-    width: 60,
-    height: 60,
   },
   css: {
     label: "CSS",
     src: "/techstack/css.svg",
-    width: 50,
-    height: 50,
   },
   styledComponents: {
     label: "Styled Components",
-    src: "/techstack/styled-components.png",
-    width: 60,
-    height: 60,
+    src: "/techstack/styled-components.svg",
   },
   cssModules: {
     label: "Css Modules",
     src: "/techstack/css-modules.webp",
-    width: 70,
-    height: 70,
   },
 };
 
 // const logos: Logos = {
 //   typescript: {
 //     label: "TypeScript",
-//     src: `/techstack/${path}/`,
-//     width: 60,
-//     height: 60,
+//     src: "/techstack/typescript.svg",
 //   },
 //   react: {
 //     label: "React",
 //     src: "/techstack/react.png",
-//     width: 60,
-//     height: 60,
 //   },
 //   nextjs: {
 //     label: "Next.js",
 //     src: "/techstack/nextjs.svg",
-//     width: 60,
-//     height: 60,
 //   },
 //   javascript: {
 //     label: "JavaScript",
 //     src: "/techstack/javascript.svg",
-//     width: 60,
-//     height: 60,
 //   },
 //   tailwind: {
 //     label: "Tailwind",
 //     src: "/techstack/tailwind-css.svg",
-//     width: 60,
-//     height: 60,
 //   },
 //   reactQuery: {
 //     label: "React Query",
 //     src: "/techstack/react-query.svg",
-//     width: 60,
-//     height: 60,
 //   },
 //   supabase: {
 //     label: "Supabase",
 //     src: "/techstack/supabase.svg",
-//     width: 60,
-//     height: 60,
 //   },
 //   redux: {
 //     label: "Redux",
 //     src: "/techstack/redux.svg",
-//     width: 60,
-//     height: 60,
 //   },
 //   reactRouter: {
 //     label: "React Router",
 //     src: "/techstack/react-router.svg",
-//     width: 60,
-//     height: 60,
 //   },
 //   git: {
 //     label: "Git",
 //     src: "/techstack/git.svg",
-//     width: 60,
-//     height: 60,
 //   },
-//   html: {
+//   framerMotion: {
 //     label: "Framer Motion",
 //     src: "/public/techstack/framer-motion.svg",
-//     width: 60,
-//     height: 60,
 //   },
 //   css: {
 //     label: "CSS",
 //     src: "/techstack/css.svg",
-//     width: 60,
-//     height: 60,
 //   },
 //   styledComponents: {
 //     label: "Styled Components",
 //     src: "/techstack/styled-components.png",
-//     width: 60,
-//     height: 60,
 //   },
 //   cssModules: {
 //     label: "Css Modules",
 //     src: "/techstack/css-modules.webp",
-//     width: 60,
-//     height: 60,
 //   },
 // };
 
