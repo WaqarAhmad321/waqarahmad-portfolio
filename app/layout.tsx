0609be48c55745b2e02db962210e1731cf38adb1
Waqar Ahmad <codewithwaqarahmad@gmail.com>
2023-11-26 14:53:32 +0500

    Update metadata title and add OpenGraph image

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,22 @@ import type { Metadata } from "next";
 import { Navbar } from "@/components";
 
 export const metadata: Metadata = {
-  title: "Waqar Ahmad",
+  title: "Waqar Ahmad - Web devloper",
   description:
     "Portfolio of Waqar Ahmad, a professional web developer specializing in Next.js, React, and TypeScript",
   metadataBase: new URL("https://waqarcodes.tech/"),
-  
+  openGraph: {
+    type: "website",
+    images: [
+      {
+        url: "https://waqarcodes.tech/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Waqar Ahmad",
+      },
+    ],
+
+  },
 };
 
 export default function RootLayout({
