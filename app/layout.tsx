322efa12c529c4112a90b9dd5008a01976b819ae
Waqar Ahmad <codewithwaqarahmad@gmail.com>
2023-11-29 10:01:03 +0500

    Update Twitter metadata in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,18 +14,11 @@ export const metadata: Metadata = {
     title: "Waqar Ahmad - Web devloper",
     description:
       "Portfolio of Waqar Ahmad, a professional web developer specializing in Next.js, React, and TypeScript",
-    images: [
-      {
-        url: "https://waqarcodes.tech/images/og-image.png",
-        width: 1200,
-        height: 630,
-        alt: "Og Image Alt",
-      },
-    ],
   },
-  // twitter: {
-  //   site: "@waqar_ahmad16",
-  // },
+  twitter: {
+    site: "@waqar___",
+    card: "summary_large_image",
+  },
 };
 
 export default function RootLayout({
