87f22483a5e9711b6fc7e9d934bd102346002103
Waqar Ahmad <codewithwaqarahmad@gmail.com>
2023-12-02 15:57:27 +0500

    Update Hero component styles

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -3,7 +3,7 @@ import { HeroText, HeroContact, HeroImage } from "@/components";
 const Hero = () => {
   return (
     <div
-      className="flex h-screen w-screen flex-col px-4 pt-10 lg:flex-row lg:px-24 lg:pt-0 2xl:pt-20"
+      className="flex h-screen w-screen flex-col px-4 pt-10 lg:flex-row lg:px-24 lg:pt-5 xl:pt-10 2xl:pt-20"
       id="home"
       style={{
         backgroundImage: `url('/background/blob-scene.svg')`,
--- components/Hero/HeroText.tsx
+++ components/Hero/HeroText.tsx
@@ -9,13 +9,13 @@ const HeroText = () => {
           <span className="text-special">Hi, </span> I&apos;m Waqar
         </h1>
 
-        <h2 className="mt-4 text-3xl sm:text-4xl md:text-5xl lg:mt-8">
+        <h2 className="mt-4 text-3xl sm:text-4xl md:text-5xl lg:mt-5 xl:mt-7 2xl:mt-8">
           A Front-End Developer.
         </h2>
       </div>
 
       <p
-        className={`mt-4 w-full text-xl font-extralight leading-8 lg:mt-8 lg:w-3/4 lg:text-2xl lg:leading-7 2xl:leading-9 xl:w-2/3 ${inter.className} text-gray-600`}
+        className={`mt-4 w-full text-xl font-extralight leading-8 lg:mt-6 xl:mt-7 2xl:mt-8  lg:w-4/5 2xl:w-2/3 lg:text-xl lg:leading-7 xl:leading-8 2xl:text-2xl 2xl:leading-9 ${inter.className} text-gray-600`}
       >
         I enjoy solving problems. Being detailed-oriented and proficient in
         modern frontend technologies, I can be relied upon to help your company
