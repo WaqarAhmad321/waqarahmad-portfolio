90d748033c9e3d37ebac0cc3de2b4383554e14ad
Waqar Ahmad <codewithwaqarahmad@gmail.com>
2024-08-20 07:59:41 +0500

    Fix merger related issue

--- components/Projects/ProjectsTitle.tsx
+++ components/Projects/ProjectsTitle.tsx
@@ -9,15 +9,3 @@ const ProjectsTitle: FC<{ title: string }> = ({ title }) => {
 };  
 
 export default ProjectsTitle;
-
-import React, { FC } from "react";
-
-const ProjectsTitle: FC<{ title: string }> = ({ title }) => {
-  return (
-    <h3 id="projects" className="text-center text-7xl font-bold capitalize text-heading-color md:text-8xl mt-8">
-      {title}
-    </h3>
-  );
-};  
-
-export default ProjectsTitle;
